ec806dfe0b492fb6854f08ed0a5b411f418926af
Fugitoid <mdemerin@gmail.com>
2015-06-16 09:12:15 -0400

    Autojoin script

---
+++ autojoin.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name Steam Monster Summer Game 2015 Autojoin Script 
+// @namespace https://github.com/Fugitoid/SMSG2015
+// @description A script that automates joining a specific room for the Steam Monster Summer Game 2015 for you.
+// @version 1.2.5
+// @match *://steamcommunity.com/minigame/
+// @match *://steamcommunity.com//minigame/
+// @grant none
+// @updateURL https://raw.githubusercontent.com/Fugitoid/SMSG2015/master/autojoin.user.js
+// @downloadURL https://raw.githubusercontent.com/Fugitoid/SMSG2015/master/autojoin.user.js
+// ==/UserScript==
+
+g_gameId = "41671";
+g_apiUrl = "http://steamapi-a.akamaihd.net/ITowerAttackMiniGameService/GetGameData/v0001/?gameid=" + g_gameId + "&include_stats=1";
+g_intervalId = 0;
+
+function success(data) {
+    if (data.response.stats.num_players < 1500) {
+        clearInterval(g_intervalId);
+        JoinGame(g_gameId);
+    }
+}
+
+g_intervalId = setInterval(function(){ jQuery.getJSON(g_apiUrl, success) }, 5);
